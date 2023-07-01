b49eb647500e7709527905cc9ae864d4b4adf66d
joseyzambranov <joseyzambranovpe@gmail.com>
2023-07-01 08:00:57 -0500

    modified domingo

--- src/app/components/reserve/calendar-full/calendar-full.component.ts
+++ src/app/components/reserve/calendar-full/calendar-full.component.ts
@@ -56,7 +56,7 @@ export class CalendarFullComponent implements OnInit {
     },
     initialView: 'timeGridDay',
     events: [],
-    hiddenDays: this.getHiddenDays(),
+    //hiddenDays: this.getHiddenDays(),
     weekends: true,
     editable: false,
     selectable: true,
